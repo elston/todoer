40607c468fbf60337640073bb2a09b7b21ee0594
Den Elston <4665284@gmail.com>
2017-09-22 16:26:30 +0300

    ...

--- images/backer/src/app/config.js
+++ images/backer/src/app/config.js
@@ -33,6 +33,7 @@ export const dbConfig = {
 */
 const EMAIL_USER = process.env.EMAIL_USER
 const EMAIL_PASS = process.env.EMAIL_PASS
+
 /**********************
 *   kubcenter.ru
 ***********************/
